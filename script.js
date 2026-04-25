d82bf5a0a150a3fe3866fa3eafa793d638ade9a2
Reed89499 <reed89499@gmail.com>
2026-04-25 09:58:17 -0400

    Added genComputerChoice and getUserChoice funcs
    Added functions to get computer choice and get
    the user's choice

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hello world');
+function genComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice = '';
+    if (randomNumber == 0) {
+        computerChoice = 'rock';
+    }   else if (randomNumber == 1) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+    return computerChoice;
+}
+
+function getUserChoice() {
+    userChoice = '';
+    while (userChoice != 'rock' || userChoice != 'paper' || userChoice != 'scissors') {
+        userChoice = prompt("Please enter rock, paper or scissors.");
+        if (userChoice == 'rock' || userChoice == 'paper' || userChoice == 'scissors') {
+            return userChoice
+        } else {
+            console.log('You did not enter rock, paper or scissors');
+        }
+    }
+}
+
+one = genComputerChoice();
+console.log(one);
+two = getUserChoice();
+console.log(two);
